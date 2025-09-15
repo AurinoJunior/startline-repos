abce4a84681e74f4fbfcf340e1b3b63da87a6b19
Aurino Geraldo <aurinoaj@gmail.com>
2025-09-15 17:33:12 -0300

    feat: initialize vite-tailwind-v4 project structure

---
+++ react/vite-tailwind-v4/src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <main className="h-screen w-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-bold text-white">Hello World!</h1>
+    </main>
+  )
+}
